4f383498b7b6e96dc31446f168a8bc14f0b5a8b6
RomanSur1998 <123935997+RomanSur1998@users.noreply.github.com>
2024-01-08 17:57:34 +0600

    add produt data object

---
+++ src/helpers/productData.js
@@ -0,0 +1,343 @@
+export const productData = {
+  product: [
+    {
+      id: 1,
+      isFavor: false,
+      files: [
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 2,
+      isFavor: false,
+      files: [
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 3,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 4,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 5,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 6,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 7,
+      isFavor: false,
+      files: [
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 8,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 9,
+      isFavor: false,
+      files: [
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 10,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 11,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 12,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 13,
+      isFavor: false,
+      files: [
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 14,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 15,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 16,
+      isFavor: false,
+      files: [
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+  ],
+  myProduct: [
+    {
+      id: 1,
+      isFavor: false,
+      files: [
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 2,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 3,
+      isFavor: false,
+      files: [
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+        "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=cover,width=1920,height=1029/sites/default/files/article_images/4u4a5328.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 4,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+    {
+      id: 5,
+      isFavor: false,
+      files: [
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+        "https://i.pinimg.com/564x/65/50/20/655020543cf6c0db794893cd220136f2.jpg",
+      ],
+      reiting: 100,
+      price: 1200,
+      name: "BMW M4 Coupe: A Two-Door",
+      description:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+      fullDescription:
+        "The BMW M4 Coupe is a high-performance version of the BMW M4 roadster and offers an exceptional",
+    },
+  ],
+  favoritesProduct: [],
+};
